e4244ecdce486aa04f44cd459d882f888a4726fb
Brian Bui <bui.brian99@gmail.com>
2025-06-20 12:37:43 -0500

    Add double jump functionality to player

--- first-game/scripts/player.gd
+++ first-game/scripts/player.gd
@@ -5,15 +5,22 @@ const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+var jump_count = 0
+var max_jumps = 2
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	# Reset jump counter
+	if is_on_floor():
+		jump_count = 0
+
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and (jump_count < max_jumps):
 		velocity.y = JUMP_VELOCITY
+		jump_count += 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
